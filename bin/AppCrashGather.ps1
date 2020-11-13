f67ec65d0675634a6a064cea40dbe2293aad96a2
dstaulcu <dstaulcu@users.noreply.github.com>
2020-11-13 17:40:31 -0500

    Update AppCrashGather.ps1

--- bin/AppCrashGather.ps1
+++ bin/AppCrashGather.ps1
@@ -1,5 +1,20 @@
 
 $CrashReportAgeThreshold = 7
+$DebugPreference = "Continue"
+
+
+# if bookmark folder does not exist, create it
+$SupportFolder = "$($env:ProgramData)\AppCrashGather"
+if (!(Test-Path -Path $SupportFolder)) { New-Item -Path $SupportFolder -ItemType Directory | Out-Null }
+
+# if bookmark file exists, get the date it contains
+$BookmarkFile = "$($SupportFolder)\LastScanDate.txt"
+if (Test-Path -Path $BookmarkFile) {
+    [datetime]$BookmarkDate = Get-Content -Path $BookmarkFile
+} else {
+    $BookmarkDate =  Get-Date -Date "01/01/1970"
+}
+
 
 # function to transform datetime objet to a string splunk can easily consume
 function format-splunktime {
@@ -13,7 +28,6 @@ function format-splunktime {
     $outputDateString  = "$($inputDateParts[0]) $($inputDateParts[1]) $($inputDateZone)"
     return $outputDateString
 }
-
 
 
 $UserProfiles = Get-WmiObject -Class Win32_UserProfile | ?{$_.SID.length -gt 8} | Select SID, LocalPath, LastUseTime, Loaded
@@ -31,44 +45,121 @@ foreach ($UserProfile in $UserProfiles)
         $user = $User -replace "\..*",""
         $user = $user.ToUpper()
 
-        $extraFiles = Get-ChildItem -path $PendingPath -Filter "*.extra"
+        $extraFiles = Get-ChildItem -path $PendingPath -Filter "*.extra" | Where-Object {$_.CreationTime -gt $BookmarkDate }
         foreach ($extraFile in $extraFiles) 
         {
             # todo - add logic to process files since last bookmark for profile path; or do alex's idea and just delete after processing.
 
-            write-debug "processing file $($extraFile.name)..."
+            write-debug "processing file $($extraFile.name) with create date $($extrafile.CreationTime)..."
             # get file content into object               
             $content = $extraFile | Get-Content
 
-            if ($content -match "^AdapterDeviceID=")
+            # initialize the event object
+            $Event=@{}
+            $eventParsed = $false
+
+            # test to see if the whole file is JSON
+            try 
+            { 
+                $content | ConvertFrom-Json | Out-Null
+                $json = $true
+            } 
+            catch 
+            {
+                $json = $false
+            }
+
+            # process potential variations of json file
+            if ($json -eq $true) 
             {
 
-                $CrashTime = (($content -match "^CrashTime=") -split "^CrashTime=")[1]
-                $CrashTimeSplunk = (Get-Date 01.01.1970).ToLocalTime()+([System.TimeSpan]::fromseconds($CrashTime))
-                $CrashTimeSplunk = format-splunktime -inputDate $CrashTimeSplunk
+                $content = $content | ConvertFrom-Json
 
-                $StartupTime = (($content -match "^StartupTime=") -split "^StartupTime=")[1]
-                $UptimeTS = (($content -match "^UptimeTS=") -split "^UptimeTS=")[1]
-                if ($UptimeTS -le 60) { $StartupCrash = "TRUE" } else { $StartupCrash = "FALSE" }
+                # extract event if the mainstream schema was found
+                if ($content.AdapterDeviceID -and $content.StackTraces -and $content.TelemetryEnvironment) 
+                {
 
-                $url = (($content -match "^URL=") -split "^URL=")[1]
-                $version = (($content -match "^Version=") -split "^Version=")[1]
-                $Addons = (($content -match "^Add-ons=") -split "^Add-ons=")[1]
-                $SecondsSinceLastCrash = (($content -match "^SecondsSinceLastCrash=") -split "^SecondsSinceLastCrash=")[1]
-                $StackTraces = (($content -match "^StackTraces=") -split "^StackTraces=")[1] | ConvertFrom-Json
+                    Write-Debug "signposts of mainstream schema detected"
 
-                $ThreadIdNameMapping = (($content -match "^ThreadIdNameMapping=") -split "^ThreadIdNameMapping=") -split ","
-                $TelemetryEnvironment = (($content -match "^TelemetryEnvironment=") -split "^TelemetryEnvironment=")  | ConvertFrom-Json
-                
-                # Find module filename whose start address is equal to or just above address of crash
-                $NearestModule = "Unknown"
-                foreach ($module in $StackTraces.modules | Sort-Object $module.base_addr) {
-                    if ($StackTraces.crash_info.address -ge $module.base_addr) {
-                        $NearestModule = $module.filename
-                        break
+                    $eventParsed = $true
+
+                    $CrashTime = $content.CrashTime
+                    $CrashTimeSplunk = (Get-Date 01.01.1970).ToLocalTime()+([System.TimeSpan]::fromseconds($CrashTime))
+                    $CrashTimeSplunk = format-splunktime -inputDate $CrashTimeSplunk
+
+                    $StartupTime = $content.startuptime
+                    $UptimeTS = $content.UptimeTS
+                    if ($UptimeTS -le 60) { $StartupCrash = "TRUE" } else { $StartupCrash = "FALSE" }
+
+                    $url = $content.url
+                    $Addons = $content.'add-ons'
+                    $SecondsSinceLastCrash = $content.SecondsSinceLastCrash
+
+                    $StackTraces = $content.StackTraces
+
+                    $ThreadIdNameMapping = ($content.ThreadIdNameMapping) -split ","
+
+                    $TelemetryEnvironment = $content.TelemetryEnvironment | ConvertFrom-Json
+                                
+                    # Find module filename whose start address is equal to or just above address of crash
+                    $NearestModule = "Unknown"
+                    foreach ($module in $StackTraces.modules | Sort-Object base_addr) {
+                        if ($StackTraces.crash_info.address -ge $module.base_addr) {
+                            $NearestModule = $module.filename
+                            break
+                        }
                     }
                 }
+
+            }
+            else 
+            {                
+            
+                # process potential variations of non-json format
+
+                # extract event if the legacy schema was found
+                if ($content -match "^AdapterDeviceID=" -and $content -match "^StackTraces=" -and $content -match "^TelemetryEnvironment=")
+                {
+
+                    Write-Debug "signposts of legacy schema detected"
+
+                    $eventParsed = $true
+
+                    $CrashTime = (($content -match "^CrashTime=") -split "^CrashTime=")[1]
+                    $CrashTimeSplunk = (Get-Date 01.01.1970).ToLocalTime()+([System.TimeSpan]::fromseconds($CrashTime))
+                    $CrashTimeSplunk = format-splunktime -inputDate $CrashTimeSplunk
+
+                    $StartupTime = (($content -match "^StartupTime=") -split "^StartupTime=")[1]
+                    $UptimeTS = (($content -match "^UptimeTS=") -split "^UptimeTS=")[1]
+                    if ($UptimeTS -le 60) { $StartupCrash = "TRUE" } else { $StartupCrash = "FALSE" }
+
+                    $url = (($content -match "^URL=") -split "^URL=")[1]
+                    $Addons = (($content -match "^Add-ons=") -split "^Add-ons=")[1]
+                    $SecondsSinceLastCrash = (($content -match "^SecondsSinceLastCrash=") -split "^SecondsSinceLastCrash=")[1]
+                    $StackTraces = (($content -match "^StackTraces=") -split "^StackTraces=")[1] | ConvertFrom-Json
+
+                    $ThreadIdNameMapping = (($content -match "^ThreadIdNameMapping=") -split "^ThreadIdNameMapping=") -split ","
+                    $TelemetryEnvironment = (($content -match "^TelemetryEnvironment=") -split "^TelemetryEnvironment=")  | ConvertFrom-Json
                 
+                    # Find module filename whose start address is equal to or just above address of crash
+                    $NearestModule = "Unknown"
+                    foreach ($module in $StackTraces.modules | Sort-Object base_addr) {
+                        if ($StackTraces.crash_info.address -ge $module.base_addr) {
+                            $NearestModule = $module.filename
+                            break
+                        }
+                    }
+                
+                }
+
+            }
+
+
+            # if we have extracted data, write out the event
+            if ($eventParsed -eq $true) {
+
+                Write-Debug "assembling report hash"
+
                 $Event = "$($CrashTimeSplunk) -"
                 $Event += " Application=`"$($Application)`""
                 $Event += " version=`"$($TelemetryEnvironment.build.displayVersion)`""
@@ -79,23 +170,32 @@ foreach ($UserProfile in $UserProfiles)
                 $Event += " StartupTime=`"$($StartupTime)`""
                 $Event += " StartupCrash=`"$($StartupCrash)`""
                 $Event += " UptimeTS=`"$($UptimeTS)`""
-                $Event += " url=`"$($url)`""
+                $Event += " url=`"$($URL)`""
                 $Event += " SecondsSinceLastCrash=`"$($SecondsSinceLastCrash)`""
                 $Event += " crash_info_address=`"$($StackTraces.crash_info.address)`""
                 $Event += " crash_info_thread=`"$($StackTraces.crash_info.crashing_thread)`""
                 $Event += " crash_info_type=`"$($StackTraces.crash_info.type)`""
-                $Event += " crash_address_module='`"$($NearestModule)`""
+                $Event += " crash_address_module=`"$($NearestModule)`""
                 $Event += " Addons=`"$($Addons)`""
 
                 Write-Output $Event
 
             }
-            else 
-            {
-                write-debug "this file is from version having unknown structure"
-            }
+        }
+
+        # if file is older than threshold, remove
+        if ((New-TimeSpan -Start $extraFile.CreationTime).TotalDays -ge $CrashReportAgeThreshold) 
+        {
+            $dmpFullName = $extraFile.FullName -replace ".extra$",".dmp"
+            if (Test-Path -Path $dmpFullName) { Remove-Item -Path $dmpFullName -Force -WhatIf }
+            if (Test-Path -Path $extraFile.FullName) { Remove-Item -Path $extraFile.FullName -Force -WhatIf }
+        }
+        else
+        {
+            Write-Debug "$($extraFile.FullName) is newer than threshold age; keeping."
         }
     }   
 }
 
-
+# write-out a bookmark file indicating that we have been here before
+Set-Content -Path $BookmarkFile -Value (Get-Date)
